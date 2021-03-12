0d36a16e2a8d4b0d79584870a76f80900afa6ff7
alonsopf <fernando.alonso@ordermygear.com>
2021-03-12 10:03:53 -0600

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,6 +2,9 @@
 describe('My test phase 1', () => {
   it('test from - to', () => {
     cy.visit("https://google.com")
-    cy.get('a[target=_top]').should('contain', 'Iniciar')
+    //cy.get('a[target=_top]').should('contain', 'Iniciar')
+
+    //due the ip server :)
+    cy.get('div').should('contain', 'United Kingdom')
   })
 })
